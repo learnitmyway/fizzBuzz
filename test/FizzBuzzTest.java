67e1c98536d59f4d137d562acb20e0ae0c11fc13
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-12-22 17:09:49 +0100

    Inline output and rename test

--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -6,12 +6,9 @@ import static org.junit.Assert.*;
 public class FizzBuzzTest {
 
     @Test
-    public void shouldProcessInput() {
+    public void shouldProcessNumber() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-
-        String output = fizzBuzz.processNumber(1);
-
-        assertThat(output, is("1"));
+        assertThat(fizzBuzz.processNumber(1), is("1"));
     }
 
 }
